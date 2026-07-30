4f31849392a63f28eba4a7f787d90b99ee5b538d
JMCFI PostFlow <postflow@jmcfi.edu.ph>
2026-07-30 12:22:44 +0800

    Commit

---
+++ backend/test_upload.php
@@ -0,0 +1,1 @@
+<?php require 'vendor/autoload.php'; \ = require_once 'bootstrap/app.php'; \ = \->make(Illuminate\Contracts\Console\Kernel::class); \->bootstrap(); \ = Illuminate\Http\UploadedFile::fake()->image('test.jpg'); \ = \->storeAs('test-dir', 'test.jpg', 's3'); var_dump(\);
