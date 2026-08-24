cb89143799dae24937e4211eb639ca2b2cf72718
unknown <Nycopaderayon03@users.noreply.github.com>
2026-08-24 22:14:08 +0800

    Remove generated API token files

--- backend/make_token.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$token = \App\Models\User::first()->createToken('Test')->plainTextToken;
-file_put_contents('token.txt', $token);
